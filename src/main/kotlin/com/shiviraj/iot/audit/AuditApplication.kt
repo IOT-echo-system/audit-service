942a9fb5099fb63e393f159ff0b03f005ae1f42f
Shivam Rajput <shivam.rajput@thoughtworks.com>
2024-01-14 15:37:57 +0530

    Shivi | update jar files

--- src/main/kotlin/com/shiviraj/iot/audit/AuditApplication.kt
+++ src/main/kotlin/com/shiviraj/iot/audit/AuditApplication.kt
@@ -1,16 +1,13 @@
 package com.shiviraj.iot.audit
 
-import com.shiviraj.iot.mqtt.config.MqttConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
-import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.ComponentScan
 
 @SpringBootApplication
 @ComponentScan(basePackages = ["com.shiviraj.iot.utils", "com.shiviraj.iot.mqtt"])
 @ConfigurationPropertiesScan
-@EnableConfigurationProperties(MqttConfig::class)
 class AuditApplication {
     companion object {
         @JvmStatic
